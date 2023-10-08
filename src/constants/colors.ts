6347fc4a30180dff40a6db307212160199356a43
Eryk Sandecki <eryk.sandecki@gmail.com>
2023-10-08 14:48:26 +0200

    main: init

---
+++ src/constants/colors.ts
@@ -0,0 +1,38 @@
+export const COMMON_COLORS = {
+  blue1: '#035afc',
+  blue2: '#8ab4f8',
+  green1: '#28a745',
+  green2: '#A7B45D',
+  netural1: '#ffffff',
+  neutral2: '#929292',
+  orange1: '#DF9862',
+  orange2: '#FDB32A',
+  red1:'#D93025',
+  skyBlue1:'#77B3CD',
+  violet1: '#4660F9'
+};
+
+export const DARK_COLORS = {
+  neutral1: '#ffffff',
+  neutral2: '#b9bbbe',
+  neutral3: '#343C43',
+  neutral4: '#1E262F',
+  neutral5: '##151D25'
+};
+
+export const LIGHT_COLORS = {
+  neutral1: '#1e2839',
+  neutral2: '#757575',
+  neutral3: '#E8E9EB',
+  neutral4: '#F1F2F6',
+  neutral5: '#FFFFFF'
+};
+
+export const FORM_COLORS = {
+  disabledDark: '#FFFFFFF80',
+  disabledLight: '#1B212880',
+  error: '#d93025',
+  errorFocus: '#B20C01',
+  labelDark: '#FFFFFF',
+  labelLight: '#1A2027'
+};
---
+++ src/constants/constants.ts
@@ -0,0 +1,14 @@
+export const CHARACTERS =
+  'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+export const DATE_FORMAT = 'DD-MM-YYYY';
+export const DATE_FORMAT_HOURS_MINUTES = 'DD-MM-YYYY HH:mm';
+export const DATE_FORMAT_HOURS_MINUTES_SECONDS = 'DD-MM-YYYY HH:mm:ss';
+
+export const DATE_FORMATS = [
+  DATE_FORMAT,
+  DATE_FORMAT_HOURS_MINUTES,
+  DATE_FORMAT_HOURS_MINUTES_SECONDS,
+] as const;
+
+export const ROWS_PER_PAGE = [5, 25, 50, 100] as const;
