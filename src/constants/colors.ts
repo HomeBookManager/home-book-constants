d77d8302114ff80710bb28f0b9898ec390fcdda9
Eryk Sandecki <eryk.sandecki@gmail.com>
2023-10-24 21:06:56 +0200

    main: refactor

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -7,26 +7,26 @@ export const COMMON_COLORS = {
   neutral2: '#929292',
   orange1: '#DF9862',
   orange2: '#FDB32A',
-  red1:'#D93025',
-  skyBlue1:'#77B3CD',
-  violet1: '#4660F9'
-};
+  red1: '#D93025',
+  skyBlue1: '#77B3CD',
+  violet1: '#4660F9',
+} as const;
 
 export const DARK_COLORS = {
   neutral1: '#ffffff',
   neutral2: '#b9bbbe',
   neutral3: '#343C43',
   neutral4: '#1E262F',
-  neutral5: '##151D25'
-};
+  neutral5: '##151D25',
+} as const;
 
 export const LIGHT_COLORS = {
   neutral1: '#1e2839',
   neutral2: '#757575',
   neutral3: '#E8E9EB',
   neutral4: '#F1F2F6',
-  neutral5: '#FFFFFF'
-};
+  neutral5: '#FFFFFF',
+} as const;
 
 export const FORM_COLORS = {
   disabledDark: '#FFFFFFF80',
@@ -34,5 +34,5 @@ export const FORM_COLORS = {
   error: '#d93025',
   errorFocus: '#B20C01',
   labelDark: '#FFFFFF',
-  labelLight: '#1A2027'
-};
+  labelLight: '#1A2027',
+} as const;
