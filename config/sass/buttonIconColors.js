ab46482ab44a20ca39f2d65ef0b4b48c99989966
Eryk Sandecki <eryk.sandecki@gmail.com>
2024-02-22 11:21:01 +0100

    main: update button icon colors

--- config/sass/buttonIconColors.js
+++ config/sass/buttonIconColors.js
@@ -1,4 +1,6 @@
 module.exports = {
+  '$--cth-btn-icon-background-click': '#000000',
+  '$--cth-btn-icon-background-click--dark': '#ffffff',
   '$--hb-btn-icon-background-hover': '#DBDCDE',
   '$--hb-btn-icon-background-hover--dark': '#3A4046',
 };
--- src/constants/scss/buttonIconColors.ts
+++ src/constants/scss/buttonIconColors.ts
@@ -1,6 +1,8 @@
 import buttonColors from '../../../config/sass/buttonIconColors';
 
 export const BUTTON_ICON_COLORS = {
+  backgroundClick: buttonColors['$--cth-btn-icon-background-click'],
+  backgroundClickDark: buttonColors['$--cth-btn-icon-background-click--dark'],
   backgroundHover: buttonColors['$--hb-btn-icon-background-hover'],
   backgroundHoverDark: buttonColors['$--hb-btn-icon-background-hover--dark'],
 };
